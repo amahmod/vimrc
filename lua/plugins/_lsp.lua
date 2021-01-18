b161b57109bbd7533ffd3e1d6ab569f04defe561
Mohammad Apel Mahmod <>
2021-01-18 14:25:25 +0600

    setup lua-language-server

--- lua/plugins/_lsp.lua
+++ lua/plugins/_lsp.lua
@@ -4,10 +4,10 @@ local map = function(type, key, value)
   vim.fn.nvim_buf_set_keymap(0,type,key,value,{noremap = true, silent = true});
 end
 
-local on_attach = function(client, buffer)
+local on_attach = function()
 
   print("LSP started.");
-  -- completion.on_attach(client, buffer) 
+  -- completion.on_attach(client, buffer)
 
   -- GOTO mappings
   map('n','gd','<cmd>lua vim.lsp.buf.definition()<CR>')
@@ -85,23 +85,44 @@ lspconfig.cssls.setup{
   }
 }
 
-lspconfig.sumneko_lua.setup{
-  on_attach= on_attach,
+local system_name
+if vim.fn.has("mac") == 1 then
+  system_name = "macOS"
+elseif vim.fn.has("unix") == 1 then
+  system_name = "Linux"
+elseif vim.fn.has('win32') == 1 then
+  system_name = "Windows"
+else
+  print("Unsupported system for sumneko")
+end
+
+-- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
+local sumneko_root_path = '/home/amahmod/lua-language-server'
+local sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
+
+require'lspconfig'.sumneko_lua.setup {
+  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
   settings = {
     Lua = {
-      runtime = { version = "LuaJIT", path = vim.split(package.path, ';'), },
-      completion = { keywordSnippet = "Disable", },
-      diagnostics = { enable = true, globals = {
-        "vim", "describe", "it", "before_each", "after_each" },
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = vim.split(package.path, ';'),
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
       },
       workspace = {
+        -- Make the server aware of Neovim runtime files
         library = {
-          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-          [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-        }
-      }
-    }
-  }
+          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+        },
+      },
+    },
+  },
 }
 
 
