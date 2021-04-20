b506e62e420619854e57b7daf1b2e6ccf08a2e33
Mohammad Apel Mahmod <>
2021-04-20 22:24:07 +0600

    hide dotfiles in nvim-tree

--- lua/modules/nvim-tree.lua
+++ lua/modules/nvim-tree.lua
@@ -2,6 +2,7 @@ vim.g.nvim_tree_disable_netrw = 1
 vim.g.nvim_tree_indent_markers = 1 -- "0 by default, this option shows indent markers when folders are open
 vim.g.nvim_tree_follow = 1 -- "0 by default, this option allows the cursor to be updated when entering a buffer
 vim.g.nvim_tree_auto_ignore_ft = 'startify' -- "empty by default, don't auto open tree on specific filetypes.
+vim.g.nvim_tree_hide_dotfiles = 1 -- "0 by default, this option hides files and folders starting with a dot `.`
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 vim.g.nvim_tree_bindings = {
   ["<CR>"] = tree_cb("edit"),
