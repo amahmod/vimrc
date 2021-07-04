2c5eb8cba3c138ab656efc550f3caa1199861b64
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2021-07-04 16:11:28 +0600

    do not auto complete spell

--- lua/modules/compe.lua
+++ lua/modules/compe.lua
@@ -19,7 +19,7 @@ require'compe'.setup {
     vsnip = {kind = "  "},
     nvim_lsp = {kind = "  "},
     nvim_lua = {kind = "  "},
-    spell = {kind = "  "},
+    -- spell = {kind = "  "},
     tags = false,
     snippets_nvim = {kind = "  "},
     -- ultisnips = {kind = "  "},
